03a791ea40e800b98d432812e3b6201a4c8c1611
Victoria Ziegler <toriziegler@cox.net>
2022-11-08 11:21:05 -0800

    graphql query and mutation to add complete

--- index.js
+++ index.js
@@ -17,46 +17,16 @@ const hospitals = [
 ]
 
 const patients = [
-    {
-        id: 1, name: 'Robert Baratheon',
-        hospitalId: 1
-    },
-    {
-        id: 2, name: 'Tyrion Lannister',
-        hospitalId: 1
-    },
-    {
-        id: 3, name: 'Cersei Lannister',
-        hospitalId: 1
-    },
-    {
-        id: 4, name: 'Daenerys Targaryen',
-        hospitalId: 1
-    },
-    {
-        id: 5, name: 'Dennis Reynolds',
-        hospitalId: 2
-    },
-    {
-        id: 6, name: 'Dee Reynolds',
-        hospitalId: 2
-    },
-    {
-        id: 7, name: 'Frank Reynolds',
-        hospitalId: 2
-    },
-    {
-        id: 8, name: 'Dwight Schrute',
-        hospitalId: 3
-    },
-    {
-        id: 9, name: 'Michael Scott',
-        hospitalId: 3
-    },
-    {
-        id: 10, name: 'Pam Beesly',
-        hospitalId: 3
-    }
+    { id: 1, name: 'Robert Baratheon', hospitalId: 1 },
+    { id: 2, name: 'Tyrion Lannister', hospitalId: 1 },
+    { id: 3, name: 'Cersei Lannister', hospitalId: 1 },
+    { id: 4, name: 'Daenerys Targaryen', hospitalId: 1 },
+    { id: 5, name: 'Dennis Reynolds', hospitalId: 2 },
+    { id: 6, name: 'Dee Reynolds', hospitalId: 2 },
+    { id: 7, name: 'Frank Reynolds', hospitalId: 2 },
+    { id: 8, name: 'Dwight Schrute', hospitalId: 3 },
+    { id: 9, name: 'Michael Scott', hospitalId: 3 },
+    { id: 10, name: 'Pam Beesly', hospitalId: 3 }
 ]
 
 const PatientType = new GraphQLObjectType({
@@ -123,8 +93,44 @@ const RootQueryType = new GraphQLObjectType({
     })
 })
 
+const RootMutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Root Mutation',
+    fields: () => ({
+        addPatient: {
+            type: PatientType,
+            description: 'Add a Patient',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                hospitalId: { type: GraphQLNonNull(GraphQLInt) }
+            },
+            resolve: (parents, args) => {
+                const patient = {
+                    id: patients.length + 1, name: args.name, hospitalId:
+                        args.hospitalId
+                }
+                patients.push(patient)
+                return patient
+            }
+        },
+        addHospital: {
+            type: HospitalType,
+            description: 'Add a Hospital',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) }
+            },
+            resolve: (parents, args) => {
+                const hospital = { id: hospitals.length + 1, name: args.name }
+                hospitals.push(hospital)
+                return hospital
+            }
+        }
+    })
+})
+
 const schema = new GraphQLSchema({
-    query: RootQueryType
+    query: RootQueryType,
+    mutation: RootMutationType
 })
 
 app.use('/graphql', graphqlHTTP({
